dbb773a81a14975eb6a4627c3a07ee42f97c67b4
Bruno Vianna <bruno_sv07@hotmail.com>
2024-04-08 08:59:18 -0300

    Aula 08/04/2024

--- src/app/components/admin/consultar-tarefas/consultar-tarefas.component.ts
+++ src/app/components/admin/consultar-tarefas/consultar-tarefas.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-consultar-tarefas',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './consultar-tarefas.component.html',
   styleUrl: './consultar-tarefas.component.css'
 })
-export class ConsultarTarefasComponent {
+export class ConsultarTarefasComponent implements OnInit {
+  tarefas: any[] = [];
+  
+  ngOnInit(): void {
+    
+    this.tarefas = [
+      {nome: 'Tarefa Exemplo 1', data: '08/04/2024', prioridade: 'Alta'},
+      {nome: 'Tarefa Exemplo 2', data: '09/04/2024', prioridade: 'Média'},
+      {nome: 'Tarefa Exemplo 3', data: '10/04/2024', prioridade: 'Baixa'},
+      {nome: 'Tarefa Exemplo 4', data: '11/04/2024', prioridade: 'Alta'},
+      {nome: 'Tarefa Exemplo 5', data: '12/04/2024', prioridade: 'Media'},
+      {nome: 'Tarefa Exemplo 6', data: '13/04/2024', prioridade: 'Baixa'},
+
+    ];
+  }
 
 }
--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ChartModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  graficoColunas : Chart = new Chart();
+  graficoLinhas : Chart = new Chart();
+  graficoDonut : Chart = new Chart();
+  graficoBarras : Chart = new Chart();
+  
+  ngOnInit(): void {
+    // MOC////////////////////////////////////////////////////////////
+    const dados = [
+      ['Exemplo 1',100],
+      ['Exemplo 2',150],
+      ['Exemplo 3',50],
+      ['Exemplo 4',250],
+    ];
+    const nomes = ['Exemplo 1','Exemplo 2','Exemplo 3','Exemplo 4'];
+
+    this.criarGraficoColunas(dados,nomes);
+    this.criarGraficoLinhas(dados,nomes);
+    this.criarGraficoDonut(dados,nomes);
+    this.criarGraficoBarras(dados,nomes);
+
+    //////////////////////////////////////////////////////////////////
+  }
+
+  criarGraficoColunas(dados: any[], nomes: any[]) : void {
+    this.graficoColunas = new Chart({
+      chart: { type : 'column'},
+      title: { text : 'Resumo de tarefas por tipo'},
+      subtitle: {text : 'Quantidade de tarefas por tipo selecionado.'},
+      series: [{ data: dados, type: undefined as any }],
+      xAxis: { categories: nomes },
+      legend: {enabled : false},
+      credits: {enabled : false }
+    });
+  }
+
+  criarGraficoLinhas(dados: any[], nomes: any[]) : void {
+    this.graficoLinhas = new Chart({
+      chart: { type : 'line'},
+      title: { text : 'Resumo de tarefas por periodo'},
+      subtitle: {text : 'Quantidade de tarefas cadstradas por período.'},
+      series: [{ data: dados, type: undefined as any }],
+      xAxis: { categories: nomes },
+      legend: {enabled : false},
+      credits: {enabled : false }
+    });
+  }
+
+  criarGraficoDonut(dados: any[], nomes: any[]) : void {
+    this.graficoDonut = new Chart({
+      chart: { type : 'pie'},
+      title: { text : 'Resumo de tarefas por prioridade.'},
+      subtitle: {text : 'Quantidade de tarefas cadstradas por prioridade.'},
+      plotOptions : {
+        pie : {
+          innerSize: '50%',
+          allowPointSelect: true,
+          cursor: 'pointer',
+          
+        }
+      },
+      series: [{ data: dados, type: undefined as any }],
+      legend: {enabled : false},
+      credits: {enabled : false }
+    });
+  }
+
+  criarGraficoBarras(dados: any[], nomes: any[]) : void {
+    this.graficoBarras = new Chart({
+      chart: { type : 'bar'},
+      title: { text : 'Resumo de tarefas por categoria'},
+      subtitle: {text : 'Quantidade de tarefas cadstradas por categoria.'},
+      series: [{ data: dados, type: undefined as any }],
+      xAxis: { categories: nomes },
+      legend: {enabled : false},
+      credits: {enabled : false }
+    });
+  }
 
 }
